e03d59171f7880be4556ade8080de11df370cb42
Alex Thola <alexthola@gmail.com>
2016-08-08 17:32:35 -0500

    Add files via upload

---
+++ CompilerTest/CompilerTest/helloworld.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CompilerTest
+//
+//  Created by Alexander Thola on 6/18/16.
+//  Copyright © 2016 Alexander Thola. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
---
+++ ForLoopDecrement/ForLoopDecrement/main.cpp
@@ -0,0 +1,17 @@
+//
+//  main.cpp
+//  ForLoopDecrement
+//
+//  Created by Alexander Thola on 6/22/16.
+//  Copyright © 2016 Alexander Thola. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    for (int i = 10; i > 0; --i) {
+        cout << i << endl;
+    }
+    return 0;
+}
